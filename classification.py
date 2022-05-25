f12594bbb03cc77491da0b11547dfdeda58f453c
hashara <kumarasinghe.k.a.s.h@gmail.com>
2022-05-25 06:45:08 +0530

    add resnet and xception

--- classification.py
+++ classification.py
@@ -11,19 +11,24 @@ IMAGE_SIZE = (IMAGE_WIDTH, IMAGE_HEIGHT)
 input_shape = (IMAGE_WIDTH, IMAGE_HEIGHT, 3)
 
 my_path = os.path.abspath(os.path.dirname(__file__))
+
+# define model paths
 mobileNet_model_path = os.path.join(my_path, "./models/MobileNetV2.h5")
 inceptionV3_model_path = os.path.join(my_path, "./models/InceptionV3.h5")
+resnet_model_path = os.path.join(my_path, "./models/ResNet50.h5")
+xception_model_path = os.path.join(my_path, "./models/Xception.h5")
 
 
 def predict_single(image):
     image = [image]
 
+    # load models
     model1_mobilenet = load_model(mobileNet_model_path)
     model2_inception = load_model(inceptionV3_model_path)
+    model3_resnet = load_model(resnet_model_path)
+    model4_xception = load_model(xception_model_path)
 
-    # single_model = load_model(mobileNet_model_path)
-
-    models = [model1_mobilenet, model2_inception]
+    models = [model1_mobilenet, model2_inception, model3_resnet, model4_xception]
 
     batch_size = 1
     nb_samples = 1
@@ -42,28 +47,22 @@ def predict_single(image):
         shuffle=False
     )
 
-    # predict = single_model.predict(test_single_gen, steps=np.ceil(nb_samples / batch_size)) # for single model
     preds = [model.predict(test_single_gen, steps=np.ceil(nb_samples / batch_size)) for model in models]
     preds = np.array(preds)
-    weights = [4/7, 3/7]
-#     weights = [0.4, 0.3]
+    # weights .5:.5:.1:.1
+    weights = [5 / 11, 4 / 11, 1 / 11, 1 / 11]
+
     weighted_preds = np.tensordot(preds, weights, axes=((0), (0)))
     weighted_ensemble_prediction = np.argmax(weighted_preds, axis=-1)
 
-    # df_file['category'] = np.argmax(predict, axis=-1) # for single model
     df_file['category'] = weighted_ensemble_prediction
     label_map = {0: 'Covid', 1: 'Normal', 2: 'Pneumonia'}
     df_file['category'] = df_file['category'].replace(label_map)
-    # print(predict[0])
 
     labels = ['Covid', 'Normal', 'Pneumonia']
-    # probability.append( predict[0])
-    #
-    # pd_prob = pd.DataFrame(probability)
+
     df = pd.DataFrame(
         data=weighted_preds,
-        # data=predict, # for single model
         columns=labels)
-    # return df_file['category'][0], predict[0]
+
     return df_file['category'][0], df
-    # return str(df_file['category'][0])
