ade6dcd6a94e5ecc645dccb9ac19022b09b9d1f8
hashara <kumarasinghe.k.a.s.h@gmail.com>
2023-04-16 11:14:43 +0530

    page configs

--- main.py
+++ main.py
@@ -1,9 +1,47 @@
 import tensorflow as tf
+import streamlit as st
 
 from pages.classification_page import start, local_css
+from pages.detail_page import start_detail_page
+
+PAGES = [
+    'Classification page',
+    'Detail page'
+]
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     with tf.device('/CPU:0'):
-        local_css("style.css")
-        start()
+        url_params = st.experimental_get_query_params()
+        if 'loaded' not in st.session_state:
+            if len(url_params.keys()) == 0:
+                st.experimental_set_query_params(page='Classification page')
+                url_params = st.experimental_get_query_params()
+
+            st.session_state.page = PAGES.index(url_params['page'][0])
+            st.session_state['loaded'] = False
+
+        st.sidebar.title('Chest Xray Analysis')
+
+
+        # selection = st.sidebar.radio("Go to", PAGES)
+
+        if st.session_state.page:
+            page = st.sidebar.radio('Navigation', PAGES, index=st.session_state.page)
+        else:
+            page = st.sidebar.radio('Navigation', PAGES, index=0)
+
+        st.experimental_set_query_params(page=page)
+
+        # case switch for pages
+        if page == 'Classification page':
+            st.session_state.page = 0
+            local_css("style.css")
+            start()
+        elif page == 'Detail page':
+            st.session_state.page = 1
+            start_detail_page()
+
+        # with tf.device('/CPU:0'):
+        #     local_css("style.css")
+        #     start()
---
+++ pages/detail_page.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+
+def start_detail_page():
+    st.title("Chest Xray Analysis ")
+    st.write("""
+    Hello
+    """)
